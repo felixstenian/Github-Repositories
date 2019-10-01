b0f3de0c856f5291e7ca4a7efcec20e347e622f4
Stenia Felix <steniafelix@gmail.com>
2019-09-30 23:58:06 -0300

    Adding events

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,16 +5,35 @@ import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
 
 export default class Main extends Component {
-  state = {};
+  state = {
+    newRepo: '',
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log(this.state.newRepo);
+  };
 
   render() {
+    const { newRepo } = this.state;
+
     return (
       <Container>
         <FaGithubAlt />
         <h1>Repositórios</h1>
 
-        <Form onSubmit={() => {}}>
-          <input type="text" placeholder="Adicionar repositório" />
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
 
           <SubmitButton disable>
             <FaPlus color="#FFF" size={14} />
