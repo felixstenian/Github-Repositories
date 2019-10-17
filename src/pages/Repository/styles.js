41fa0f3e88d30c5a74450742761e682d578768dc
Stenia Felix <steniafelix@gmail.com>
2019-10-17 01:52:26 -0300

    Styles botton pagination

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -147,3 +147,26 @@ export const FilterIssues = styled.ul`
     }
   }
 `;
+
+export const Pagination = styled.div`
+  padding-top: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 12px;
+
+  button {
+    transition: opacity 0.25s ease-out;
+    border-radius: 4px;
+    outline: 0;
+    border: 0;
+    padding: 8px;
+    background: #836FFF;
+    color: #fafafa;
+
+    &:disabled {
+      opacity: 0.35;
+      cursor: not-allowed;
+    }
+  }
+`;
